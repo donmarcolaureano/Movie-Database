72719caa35fe457f4e49bd88f34dd84415403c25
Marco Laureano <laureano.marco@gmail.com>
2023-10-09 12:01:19 -0700

    update

--- movie_database.sql
+++ movie_database.sql
@@ -1,6 +1,11 @@
+DROP SCHEMA IF EXISTS movie_database;
+CREATE SCHEMA IF NOT EXISTS movie_database DEFAULT CHARACTER SET utf8;
+USE movie_database;
+
 CREATE TABLE `genres`  (
-  `genre_id` int NULL,
-  `genre_name` varchar(255) NULL
+  `genre_id` int NOT NULL,
+  `genre_name` varchar(255) NULL,
+  PRIMARY KEY (`genre_id`)
 ) ENGINE = InnoDB;
 
 CREATE TABLE `ratings`  (
@@ -22,11 +27,4 @@ CREATE TABLE `title_genres`  (
   `tconst` varchar(255) NULL,
   `genre_id` int NOT NULL
 ) ENGINE = InnoDB;
-
-CREATE TABLE `tmdb_data`  (
-  `imdb_id` int NULL,
-  `revenue` int NULL,
-  `budget` int NULL,
-  `certification` varchar(255) NULL
-);
 
